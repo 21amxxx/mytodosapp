de5cf97c49710aa0d6f7d8f20f1a7ef0f35f4fa0
Amen Alahassa <am.konrad21@gmail.com>
2020-07-04 12:50:28 +0100

    Updates

---
+++ precache-manifest.d73e54e2ae666bc9820ef0f11d49c482.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a51dd0ff51a58833e014adad890d53d",
+    "url": "/mytodosapp/index.html"
+  },
+  {
+    "revision": "3453d18240c64eb32326",
+    "url": "/mytodosapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6e30e17f5906cf57caa2",
+    "url": "/mytodosapp/static/css/main.75a936c1.chunk.css"
+  },
+  {
+    "revision": "3453d18240c64eb32326",
+    "url": "/mytodosapp/static/js/2.c4464018.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/mytodosapp/static/js/2.c4464018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e30e17f5906cf57caa2",
+    "url": "/mytodosapp/static/js/main.66a0d35d.chunk.js"
+  },
+  {
+    "revision": "8d8b8526772fde8867f7",
+    "url": "/mytodosapp/static/js/runtime-main.3a3c2a02.js"
+  }
+]);
